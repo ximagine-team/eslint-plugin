e41ce9988536b761cbd0641e7b73c3a07cbd93c5
Alex Shan <shan-mx@hotmail.com>
2024-11-09 10:59:12 +0800

    feat: add no jsx-optional-chaining rule

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -3,6 +3,7 @@ import type { RuleModule } from "src/utils/create-rule";
 import enforceRequestPrefix from "./enforce-request-prefix";
 import functionParamsDestructuring from "./function-params-destructuring";
 import noJsxNonNullAssertion from "./no-jsx-non-null-assertion";
+import noJsxOptionalChaining from "./no-jsx-optional-chaining";
 import noUncaughtMutateAsync from "./no-uncaught-mutate-async";
 import noUnsafeTypeAssertion from "./no-unsafe-type-assertion";
 import noVoidMutateAsync from "./no-void-mutate-async";
@@ -21,20 +22,22 @@ export const rules = {
   "enforce-request-prefix": enforceRequestPrefix,
   "function-params-destructuring": functionParamsDestructuring,
   "no-jsx-non-null-assertion": noJsxNonNullAssertion,
+  "no-jsx-optional-chaining": noJsxOptionalChaining,
   "no-uncaught-mutate-async": noUncaughtMutateAsync,
   "no-unsafe-type-assertion": noUnsafeTypeAssertion,
   "no-void-mutate-async": noVoidMutateAsync,
-  "prefer-one-line-arrow-function": preferOneLineArrowFunction,
   "padding-lines": paddingLines,
+  "prefer-one-line-arrow-function": preferOneLineArrowFunction,
 } satisfies Record<string, RuleModule<unknown[]>>;
 
 export const ruleCategories: Record<RuleName, RuleCategory> = {
   "enforce-request-prefix": "react-query",
   "function-params-destructuring": "code-style",
   "no-jsx-non-null-assertion": "best-practice",
+  "no-jsx-optional-chaining": "best-practice",
   "no-uncaught-mutate-async": "react-query",
   "no-unsafe-type-assertion": "best-practice",
   "no-void-mutate-async": "react-query",
-  "prefer-one-line-arrow-function": "code-style",
   "padding-lines": "code-style",
+  "prefer-one-line-arrow-function": "code-style",
 };
---
+++ src/rules/no-jsx-optional-chaining.test.ts
@@ -0,0 +1,57 @@
+import { unindent as $ } from "eslint-vitest-rule-tester";
+
+import { runTest } from "../utils/run-test";
+import rule, { RULE_NAME } from "./no-jsx-optional-chaining";
+
+runTest({
+  name: RULE_NAME,
+  rule,
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  valid: [
+    // Regular JSX without optional chaining
+    "<div>{user.name}</div>",
+    "<UserProfile data={user} />",
+    // Using intermediate variables
+    $`
+      const userName = user?.name;
+      <div>{userName}</div>
+    `,
+    $`
+      const userEmail = user?.profile?.email;
+      <div>{userEmail}</div>
+    `,
+    // Non-JSX optional chaining (allowed)
+    "const name = user?.name",
+    "function getName() { return user?.name }",
+  ],
+  invalid: [
+    {
+      code: "<div>{user?.name}</div>",
+      errors: [{ messageId: "noJsxOptionalChaining" }],
+    },
+    {
+      code: "<UserProfile data={user?.data} />",
+      errors: [{ messageId: "noJsxOptionalChaining" }],
+    },
+    {
+      code: "<div title={props?.title}>Content</div>",
+      errors: [{ messageId: "noJsxOptionalChaining" }],
+    },
+    {
+      code: "<div>{user?.profile?.email}</div>",
+      errors: [
+        { messageId: "noJsxOptionalChaining" },
+      ],
+    },
+    {
+      code: "<Component {...props?.data} />",
+      errors: [{ messageId: "noJsxOptionalChaining" }],
+    },
+  ],
+}); 
