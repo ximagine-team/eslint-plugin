dd64d72b4664e0279aead2d7b9537e4796c6dd99
Alex Shan <shan-mx@hotmail.com>
2024-11-19 12:04:59 +0800

    chore: remove unused rule overrides

--- eslint.config.js
+++ eslint.config.js
@@ -11,10 +11,6 @@ export default defineConfig({
     {
       rules: {
         "unicorn/consistent-function-scoping": "off",
-        "@typescript-eslint/no-unsafe-argument": "off",
-        "@typescript-eslint/no-unsafe-member-access": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unsafe-assignment": "off",
       },
     },
   ],
